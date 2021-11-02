6efd077b2d76c85353b01579c9eab30ed80b9d45
Yu-HuanWu <yuhuanhwu@gmail.com>
2021-11-01 17:12:51 -0700

    Updates

---
+++ precache-manifest.447f7ed2cedd5ff01ad14963af23039a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe86e072ceab71a086a7cd92e035e929",
+    "url": "/Klingon_Calculator/index.html"
+  },
+  {
+    "revision": "7e57c1a9cb9a63340893",
+    "url": "/Klingon_Calculator/static/css/main.c56c3e70.chunk.css"
+  },
+  {
+    "revision": "e77c6f9015b28f864311",
+    "url": "/Klingon_Calculator/static/js/2.4ba506ce.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Klingon_Calculator/static/js/2.4ba506ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e57c1a9cb9a63340893",
+    "url": "/Klingon_Calculator/static/js/main.1db0c943.chunk.js"
+  },
+  {
+    "revision": "0c07a7d9b2e1961c4376",
+    "url": "/Klingon_Calculator/static/js/runtime-main.9aeee881.js"
+  }
+]);
