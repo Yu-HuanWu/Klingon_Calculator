6926b011a46f0195bf1c2c0de7c027900a4633c7
Yu-HuanWu <yuhuanhwu@gmail.com>
2021-11-01 20:07:40 -0700

    read me and question info

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
 # Klingon Calculator
 
+## Background
+
+
+## Technologies
+* React
+* HTML/ CSS
+
+## Note about the translation
+
+![Negative](https://raw.githubusercontent.com/Yu-HuanWu/Klingon_Calculator/6ef915ad81f53c5ec33782d1f02c0e7e3809aaf9/public/KlingonNegative.svg)
+According to the [Daily Klingon](https://daily-klingon.tumblr.com/post/177667068522/simple-mathematical-operations), negative number "can be expressed as pagh bIngDaq mI’ [number] below zero. Examples: pagh bIngDaq cha’ -2; pagh bingDaq wejvatlh wej -33." Although this may 
+or may not be canon, since creator of Klingon Marc Okrand hasn't define it yet.
+
+
+
 ## Special Thanks to:
 * Marc Okrand
 * App Academy BenchMark Exam
--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <div className="App">
       <header>
-        <img src="klingonlogo.png" className="logo" alt="App Academy" />
+        <img src="klingonlogo.png" className="logo" alt="Klingon Logo" />
       </header>
       <Calculator />
       <footer>
@@ -15,6 +15,9 @@ const App = () => {
         <div className="footer-info">
           <a href="https://github.com/Yu-HuanWu" target="_blank" title="My GitHub link" rel="noopener noreferrer">
             <img src="githublogo.gif" alt="GitHub waving gif made by Yu-Huan Wu" height="70"/>
+          </a>
+          <a href="https://github.com/Yu-HuanWu/Klingon_Calculator#readme" target="_blank" title="Info about this calculator" rel="noopener noreferrer">
+            <img src="question.svg" alt="Star Trek question mark" height="70" />
           </a>
           <a href="https://www.linkedin.com/in/yu-huan-wu" target="_blank" title="My LinkedIn. If you want to hire me just holler at me my dude" rel="noopener noreferrer">
             <img src="linkedin.gif" alt="LinkedIn bouncing gif made by Yu-Huan Wu" height="70"/>
